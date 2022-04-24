450bc1a236f3d939aeb61e0bcaa5b0c00486c3ec
Efstathios Chatzikyriakidis <stathis.chatzikyriakidis@gmail.com>
2022-04-25 00:40:44 +0300

    Change.

--- development/vehicle_parking_alarm_via_ultrasonic_distance_ping_sensor/vehicle_parking_alarm_via_ultrasonic_distance_ping_sensor.ino
+++ development/vehicle_parking_alarm_via_ultrasonic_distance_ping_sensor/vehicle_parking_alarm_via_ultrasonic_distance_ping_sensor.ino
@@ -1,7 +1,7 @@
 /*
  *  Vehicle Parking Alarm Via Ultrasonic Distance PING Sensor.
  *
- *  Copyright (C) 2010 Efstathios Chatzikyriakidis (contact@efxa.org)
+ *  Copyright (C) 2010 Efstathios Chatzikyriakidis (stathis.chatzikyriakidis@gmail.com)
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
